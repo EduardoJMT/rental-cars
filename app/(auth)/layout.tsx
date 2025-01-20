51efdd79e35dd6af9bca5ce4935d28f4ba96ef5f
Eduardo <jesus.mares.torres@hotmail.com>
2025-01-20 11:01:47 -0600

    Clerk and sign in page added

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import React from "react";
+
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="grid lg:grid-cols-2 h-full items-center justify-center">
+      <div className="flex items-center justify-center">{children}</div>
+      <div className="hidden lg:flex lg:bg-slate-300 h-full justify-center items-center lg:flex-col">
+        <Image
+          src={"/logo.svg"}
+          alt="Logo Eduardo Mares Cars"
+          width={80}
+          height={80}
+        ></Image>
+        <h1 className="text-xl font-bold">Rental cars Eduardo Mares</h1>
+      </div>
+    </div>
+  );
+}
